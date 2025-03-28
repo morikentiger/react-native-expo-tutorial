7a0018ba7a824e776134ee7077bb51d20a6bbb0c
森田健太 <moritakenta@moritakentanoMacBook-Pro.local>
2025-03-29 06:43:34 +0900

    型チェックするスクリプトを追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
